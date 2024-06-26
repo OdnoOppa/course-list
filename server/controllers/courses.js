372a1273239e9cc3d2efbafe1760dd3dd1afc9d3
Nu Pogodyy <Odontuyadorj2@gmail.com>
2024-06-26 15:04:49 +0800

    mutation done

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -1,15 +1,29 @@
 import knex from "../lib/db.js";
+import { customAlphabet } from "nanoid";
 
+const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-export async function getCoursesByClassId(classId){
-  return await knex.table("course").where({ classId });
+export async function createCourse(classId, title, description) {
+  const newCourse = {
+    id: customAlphabet(chars, 12)(),
+    classId,
+    title,
+    description,
+    createdAt: new Date().toISOString(),
+  };
+  console.log(newCourse);
+  await knex.table("course").insert(newCourse);
+  return newCourse;
+}
+
+export async function getCoursesByClassId(classId) {
+  return await knex.table("course").select().where({ classId });
 }
 
 export async function getCourses() {
   return await knex.table("course").select();
 }
 
-
 export async function getCourseById(id) {
-    return await knex.table("course").first().where({ id });
+  return await knex.table("course").first().where({ id });
 }
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,67 @@
+import { GraphQLError } from 'graphql';
+import {
+    getCourseById,
+    getCourses,
+    getCoursesByClassId,
+    createCourse,
+} from "./controllers/courses.js";
+import { getClassById } from "./controllers/class.js";
+import { getVideosByCourseId } from "./controllers/videos.js";
+
+export const resolvers = {
+
+    Mutation: {
+        createCourse: (root, { input: { title, description } }) => {
+            const classId = "FjcJCHJALA4i";
+            return createCourse(classId, title, description);
+        },
+    },
+
+/*
+    Mutation: {
+        createCourse: (root, { input: { title, description }}) => {
+            const classId = "FjcJCHJALA4i";
+            return createCourse(classId, title, description);
+        },
+    },
+
+        Mutation: {
+        createCourse: (root, { title, description }) => {
+            const classId = "FjcJCHJALA4i";
+            return createCourse(classId, title, description);
+        },
+    },
+*/
+
+    Query: {
+        courses: () => getCourses(),
+        course: (root, { id }) => getCourseById(id),
+        class: async (root, { id }) => {
+            const classData = await getClassById(id);
+            if (!classData) {
+                throw new GraphQLError(`${id} id-тай анги олдсонгүй.`, {
+                    extensions: { code: 'CLASS_NOT_FOUND' },
+                });
+            }
+            return classData;
+        },
+    },
+
+    Class: {
+        courses: (root) => getCoursesByClassId(root.id),
+    },
+
+    Course: {
+        date: (root) => root.createdAt.slice(0, "yyyy-mm-dd".length),
+        class: (root) => getClassById(root.classId),
+        videos: (root) => getVideosByCourseId(root.id),
+    },
+};
+
+
+
+/*
+import { GraphQLError } from 'graphql';
 import { getCourses, getCourseById, getCoursesByClassId } from "./controllers/courses.js";
 import { getClassById } from "./controllers/class.js";
 import { getVideosByCourseId } from "./controllers/videos.js";
@@ -6,9 +70,21 @@ import { getVideosByCourseId } from "./controllers/videos.js";
 export const resolvers = {
   Query: {
     courses: () => getCourses(),
-    course: (root, {id}) => getCourseById(id),
-    class: (root, {id}) => getClassById(id),
-    
+    course: async (root, {id}) => {
+        const course = await getCourseById(id)
+        if(!course){
+            throwNotFoundError(`${id} хичээл олдсонгүй`);
+        }
+    },
+
+
+    class: async (root, {id}) => {
+            const classData = await getClassById(id)
+            if(!classData) {
+                    throwNotFoundError(`${id} анги олдсонгүй`);
+                    }
+            return classData;
+    },
   },
 
  Class: {
@@ -22,3 +98,13 @@ export const resolvers = {
     videos: (root) => getVideosByCourseId(root.id),
   },
 };
+
+
+function throwNotFoundError(message){
+    throw new GraphQLError(message, {
+        extensions: { code: 'BAD_USER_INPUT'},
+    });
+}
+
+
+*/
